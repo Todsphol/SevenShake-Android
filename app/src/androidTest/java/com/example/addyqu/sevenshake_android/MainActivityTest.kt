57483a896e1b720eed5d22b68d4862999740ea1e
Todsphol <theaddza11@gmail.com>
2018-04-07 00:29:02 +0700

    Change theme color and create new editext test

--- app/src/androidTest/java/com/example/addyqu/sevenshake_android/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/addyqu/sevenshake_android/MainActivityTest.kt
@@ -1,16 +1,16 @@
 package com.example.addyqu.sevenshake_android
 
-import android.support.test.InstrumentationRegistry
+
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions
+import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 import org.junit.Rule
 
 @RunWith(AndroidJUnit4::class)
@@ -19,8 +19,14 @@ class MainActivityTest {
     val activityRule =
             ActivityTestRule<MainActivity>(MainActivity::class.java, true, true)
     @Test
-    fun test_on_click_button() {
-        onView(withId(R.id.btnBlue))
+    fun test_Input_On_EdiText() {
+        onView(withId(R.id.edt_test))
+                .check(matches(withText("")))
+    }
+
+    @Test
+    fun test_On_Click_Button() {
+        onView(withId(R.id.btn_test))
                 .perform(ViewActions.click())
     }
 }
