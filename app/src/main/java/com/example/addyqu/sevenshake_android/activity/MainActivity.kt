3fd861be5a149af47277405b96fa48024e60f8fe
Todsphol <theaddza11@gmail.com>
2018-04-22 23:52:29 +0700

    add dependency kapt

--- app/src/main/java/com/example/addyqu/sevenshake_android/activity/MainActivity.kt
+++ app/src/main/java/com/example/addyqu/sevenshake_android/activity/MainActivity.kt
@@ -3,12 +3,12 @@ package com.example.addyqu.sevenshake_android.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.addyqu.sevenshake_android.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
