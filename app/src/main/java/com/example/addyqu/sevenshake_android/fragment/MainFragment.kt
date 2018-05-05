67004607fb748afcbb692a47f7c846afc674911e
Todsphol <theaddza11@gmail.com>
2018-05-05 22:06:42 +0700

    Adjust fragment main xml

--- app/src/main/java/com/example/addyqu/sevenshake_android/fragment/MainFragment.kt
+++ app/src/main/java/com/example/addyqu/sevenshake_android/fragment/MainFragment.kt
@@ -6,16 +6,15 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.example.addyqu.sevenshake_android.R
-
-// TODO: Rename parameter arguments, choose names that match
 
 class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+         val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+            return view
     }
 
 
